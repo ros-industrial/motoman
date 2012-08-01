7426a7af15bb9a1d0d8c734597f2c1ea189e324a
shaun-edwards <shaun.edwards>
2012-08-01 00:09:54 +0000

    Updated joint_trajectory_downloader.cpp to output trajectory job on the client (ROS) side instead of sending it to the controller.
    Updated filter server launch file to point to local (longhorn) yaml config file.
    
    git-svn-id: https://swri-ros-pkg.googlecode.com/svn/trunk@430 076cdf4d-ed99-c8c1-5d01-9bc0d27e81bd

--- dx100/src/joint_trajectory_downloader.cpp
+++ dx100/src/joint_trajectory_downloader.cpp
@@ -30,18 +30,26 @@
  */
 
 #include <dx100/joint_trajectory_downloader.h>
+#include "dx100/trajectory_job.h"
+#include "simple_message/joint_traj.h"
 #include "simple_message/joint_traj_pt.h"
 #include "simple_message/messages/joint_traj_pt_message.h"
 #include "simple_message/smpl_msg_connection.h"
 #include <dx100/utils.h>
 
+#include <fstream>
+
 using namespace industrial::smpl_msg_connection;
 using namespace industrial::joint_data;
+using namespace industrial::joint_traj;
 using namespace industrial::joint_traj_pt;
 using namespace industrial::joint_traj_pt_message;
 using namespace industrial::simple_message;
 using namespace motoman::utils;
+using namespace motoman::trajectory_job;
 
+//#define SEND_TRAJ_TO_ROBOT
+#define GENERATE_LOCAL_JOB_FILE
 
 namespace motoman
 {
@@ -88,12 +96,23 @@ void JointTrajectoryDownloader::jointTrajectoryCB(
 		return;
 	}
 
+#ifdef SEND_TRAJ_TO_ROBOT
+
   if (!this->robot_->isConnected())
   {
     ROS_WARN("Attempting robot reconnection");
     this->robot_->makeConnect();
   }
+#endif
   
+#ifdef GENERATE_LOCAL_JOB_FILE
+
+  JointTraj traj;
+  TrajectoryJob job;
+  job.init("jtd.job");
+
+#endif
+
   ROS_INFO("Sending trajectory points, size: %d", msg->points.size());
 
 	for (int i = 0; i < msg->points.size(); i++)
@@ -107,6 +126,7 @@ void JointTrajectoryDownloader::jointTrajectoryCB(
 		// Performing a manual copy of the joint velocities in order to send them
     // to the utility function.  Passing the pt data members doesn't seem to
     // work.
+    ROS_INFO("Performing joint velocities copy");
     std::vector<double> joint_velocities(0.0);
     double velocity =0 ;
     joint_velocities.resize(msg->joint_names.size(), 0.0);
@@ -114,7 +134,7 @@ void JointTrajectoryDownloader::jointTrajectoryCB(
     {
       joint_velocities[j] = msg->points[i].velocities[j];
     }
-    ROS_DEBUG("Joint velocities copied");
+    ROS_INFO("Joint velocities copied");
     velocity = toMotomanVelocity(joint_velocity_limits, joint_velocities);
 
     jPt.setVelocity(velocity);
@@ -123,12 +143,12 @@ void JointTrajectoryDownloader::jointTrajectoryCB(
 		// value
 		if (0 == i)
 		{
-			ROS_DEBUG("First trajectory point, setting special sequence value");
+			ROS_INFO("First trajectory point, setting special sequence value");
 			jPt.setSequence(SpecialSeqValues::START_TRAJECTORY_DOWNLOAD);
 		}
 		else if (msg->points.size() - 1 == i)
 		{
-			ROS_DEBUG("Last trajectory point, setting special sequence value");
+			ROS_INFO("Last trajectory point, setting special sequence value");
 			jPt.setSequence(SpecialSeqValues::END_TRAJECTORY);
 		}
 		else
@@ -149,7 +169,8 @@ void JointTrajectoryDownloader::jointTrajectoryCB(
 		jMsg.init(jPt);
 		jMsg.toTopic(topic);
 
-		ROS_DEBUG("Sending joint trajectory point");
+#ifdef SEND_TRAJ_TO_ROBOT
+		ROS_INFO("Sending joint trajectory point");
 		if (this->robot_->sendMsg(topic))
 		{
 			ROS_INFO("Point[%d] sent to controller", i);
@@ -158,7 +179,39 @@ void JointTrajectoryDownloader::jointTrajectoryCB(
 		{
 			ROS_WARN("Failed sent joint point, skipping point");
 		}
+#endif
+
+#ifdef GENERATE_LOCAL_JOB_FILE
+
+		ROS_INFO("Adding joint point to local trajectory");
+		traj.addPoint(jPt);
+
+#endif
+
 	}
+
+#ifdef GENERATE_LOCAL_JOB_FILE
+
+  const int JOB_BUFFER_SIZE = 500000;
+  char jobBuffer[JOB_BUFFER_SIZE];
+	job.toJobString(traj, &jobBuffer[0], JOB_BUFFER_SIZE);
+	  std::ofstream file;
+	  file.open(job.getName());
+	  if (file.is_open())
+	  {
+	  	ROS_INFO_STREAM("Writing job to file: " << job.getName());
+	  	file << jobBuffer;
+	  }
+	  else
+	  {
+	  	ROS_WARN_STREAM("Failed to open job file: " << job.getName());
+	  }
+  	file.close();
+
+#endif
+
+
+
 }
 
 void JointTrajectoryDownloader::trajectoryStop()
