befc48bb7443769b9ae9889696b40f4c9bb8d61a
Patrick Beeson <pbeeson@traclabs.com>
2017-03-15 12:29:47 -0400

    Update to motoman_driver that will not auto-reconnect (and auto turn on servos) after motion is stopped by the user or by an estop without an explicit service call into the drive from the outside.  Prior, a client that was blasted motions without paying attention to any estop signal could make the robot start moving as soon as the estop button on the teach pendant was cleared, which was unsafe.

--- motoman_driver/src/joint_trajectory_streamer.cpp
+++ motoman_driver/src/joint_trajectory_streamer.cpp
@@ -85,6 +85,11 @@ bool MotomanJointTrajectoryStreamer::init(SmplMsgConnection* connection, const s
 
     motion_ctrl_map_[robot_id] = motion_ctrl;
   }
+
+  disabler_ = node_.advertiseService("/motoman_disable", &MotomanJointTrajectoryStreamer::disableRobotCB, this);
+
+  enabler_ = node_.advertiseService("/motoman_enable", &MotomanJointTrajectoryStreamer::enableRobotCB, this);
+
   return rtn;
 }
 
@@ -104,6 +109,10 @@ bool MotomanJointTrajectoryStreamer::init(SmplMsgConnection* connection, const s
 
   rtn &= motion_ctrl_.init(connection, robot_id_);
 
+  disabler_ = node_.advertiseService("/motoman_disable", &MotomanJointTrajectoryStreamer::disableRobotCB, this);
+
+  enabler_ = node_.advertiseService("/motoman_enable", &MotomanJointTrajectoryStreamer::enableRobotCB, this);
+
   return rtn;
 }
 
@@ -113,6 +122,43 @@ MotomanJointTrajectoryStreamer::~MotomanJointTrajectoryStreamer()
   motion_ctrl_.setTrajMode(false);   // release TrajMode, so INFORM jobs can run
 }
 
+bool MotomanJointTrajectoryStreamer::disableRobotCB(std_srvs::Trigger::Request &req,
+                                           std_srvs::Trigger::Response &res)
+{
+  bool ret = motion_ctrl_.setTrajMode(false);  
+  res.success = ret;
+  
+  if (!res.success)
+    res.message="Robot was NOT disabled. Please re-examine and retry.";
+  else
+    res.message="Robot is now disabled and will NOT accept motion commands.";
+
+  ROS_WARN_STREAM(res.message);
+
+  return true;
+
+}
+
+bool MotomanJointTrajectoryStreamer::enableRobotCB(std_srvs::Trigger::Request &req,
+						   std_srvs::Trigger::Response &res)
+{
+  bool ret = motion_ctrl_.setTrajMode(true);  
+  res.success = ret;
+  
+  if (!res.success)
+    res.message="Robot was NOT enabled. Please re-examine and retry.";
+  else
+    res.message="Robot is now enabled and will accept motion commands.";
+
+
+  ROS_WARN_STREAM(res.message);
+
+  return true;
+
+}
+
+
+  
 // override create_message to generate JointTrajPtFull message (instead of default JointTrajPt)
 bool MotomanJointTrajectoryStreamer::create_message(int seq, const trajectory_msgs::JointTrajectoryPoint &pt, SimpleMessage *msg)
 {
@@ -311,12 +357,12 @@ bool MotomanJointTrajectoryStreamer::VectorToJointData(const std::vector<double>
   }
   return true;
 }
-
+  
 // override send_to_robot to provide controllerReady() and setTrajMode() calls
 bool MotomanJointTrajectoryStreamer::send_to_robot(const std::vector<SimpleMessage>& messages)
 {
-  if (!motion_ctrl_.controllerReady() && !motion_ctrl_.setTrajMode(true))
-    ROS_ERROR_RETURN(false, "Failed to initialize MotoRos motion.  Trajectory ABORTED.  Correct issue and re-send trajectory.");
+  if (!motion_ctrl_.controllerReady())
+    ROS_ERROR_RETURN(false, "Failed to initialize MotoRos motion.  Trajectory ABORTED.  Send /motoman_enable signal when safe and re-send trajectory.");
 
   return JointTrajectoryStreamer::send_to_robot(messages);
 }
