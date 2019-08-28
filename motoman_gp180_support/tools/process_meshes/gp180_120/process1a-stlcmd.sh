ef83f81a9601170567ac7f0104b79259cfac62f8
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2019-08-28 16:32:51 +0200

    Place tool0 on flange.
    So move final translation (from J5->J6) and make J6->tool0 transform identity.

--- motoman_gp180_support/tools/process_meshes/gp180_120/process1a-stlcmd.sh
+++ motoman_gp180_support/tools/process_meshes/gp180_120/process1a-stlcmd.sh
@@ -6,4 +6,4 @@ stl_transform -rx 90 -rz 180 -s 0.001 -tx -0.325 -tz -0.65 input/L_AXIS.stl outp
 stl_transform -rx 90 -rz 180 -s 0.001 -tx -0.325 -tz -1.8 input/U_AXIS.stl output_visual/link_3_u.stl
 stl_transform -rx 90 -rz 180 -s 0.001 -tx -0.325 -tz -2.1 input/R_AXIS.stl output_visual/link_4_r.stl
 stl_transform -rx 90 -rz 180 -s 0.001 -tx -1.915 -tz -2.1 input/B_AXIS.stl output_visual/link_5_b.stl
-stl_transform -rx 90 -rz 180 -s 0.001 -tx -1.915 -tz -2.1 input/T_AXIS.stl output_visual/link_6_t.stl
+stl_transform -rx 90 -rz 180 -s 0.001 -tx -2.140 -tz -2.1 input/T_AXIS.stl output_visual/link_6_t.stl
