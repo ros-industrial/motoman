e2afb2962be97e061d624d3ddffc9870900e2310
Ted Miller <ted.miller@motoman.com>
2022-01-25 09:48:34 -0500

    Fix: If alarm occurs while queue is full and there is pending data to be processed, then subsequent trajectory commands will fail.

--- motoman_driver/MotoPlus/MotoROS.h
+++ motoman_driver/MotoPlus/MotoROS.h
@@ -32,7 +32,7 @@
 #ifndef MOTOROS_H
 #define MOTOROS_H
 
-#define APPLICATION_VERSION					"1.9.9"
+#define APPLICATION_VERSION					"1.9.10"
 
 #include "MotoPlus.h"
 #include "ParameterExtraction.h"
